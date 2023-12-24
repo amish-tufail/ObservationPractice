ccd2f3ce23727c7236569a14d1cc2b21b27d09d8
Amish <amish.tufail2002@gmail.com>
2023-12-24 18:23:45 +0500

    Initial Commit

---
+++ ObservationPractice/ObservationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationPracticeApp.swift
+//  ObservationPractice
+//
+//  Created by Amish Tufail on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ObservationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
